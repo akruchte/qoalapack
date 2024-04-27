c1f8afb1d6459934fb7b92ae5789825af3a9696c
Adam <kruchten93@gmail.com>
2024-04-27 12:23:10 -0400

    Update

--- R/qoala-package.R
+++
@@ -1,8 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-#' @importFrom sf st_as_sf
-#' @importFrom tibble as_tibble
-## usethis namespace: end
-NULL
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,12 @@
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do additional test configuration?
+# Learn more about the roles of various files in:
+# * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
+# * https://testthat.r-lib.org/articles/special-files.html
+
 library(testthat)
-library(disslib)
+library(qoalapack)
 
-test_check("disslib")
-
-library(spatstat)
-p <- Pcov(swedishpines, W = swedishpines$window, dimyx = c(128, 128))
-
-
+test_check("qoalapack")
