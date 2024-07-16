82eb72c6564605e68ef71c86c1b1b1816bd0c261
akruchte <kruchten93@gmail.com>
2024-07-16 14:22:07 -0400

    Remove dead code chunks and examples

--- R/bridge.R
+++
@@ -1,9 +0,0 @@
-library(Rcpp)
-library(here)
-
-library(spatstat)
-
-x <- rpoispp(10)
-
-
-f <- Rcpp::sourceCpp(file = here("R/src/test.cpp"))
--- R/exposure_models_continuous.R
+++ R/exposure_models_continuous.R
@@ -1,18 +1,3 @@
-
-## example
-if(FALSE){
-    w <- owin()
-    obsp <- runifpoint(110)
-
-    treatment <-function(x,y) 2 * x + log(x) + x * y / 15 + 0.01 * x^2 + 0.3 * y + sin(y) + rnorm(length(x))
-    cobs <- coords(obsp)
-    treat <- treatment(cobs[,1], cobs[,2])
-
-    dd <- data.frame(treat = treat, x = cobs[,1], y = cobs[,2])
-
-    f <- gam(treat ~ s(x) + s(y), data = dd, family = gaussian())
-}
-
 ## numerator model is used for stabilization weights
 ## it takes in an exposure model as an argument and fits
 ## the model required for propensity score stabilization
@@ -41,6 +26,8 @@ exposure_model <- function(formula, data, family, method = 'gam', ..., numerator
 
     mod_fit
 }
+
+
 
 select_engine <- function(method) {
     switch(method,
